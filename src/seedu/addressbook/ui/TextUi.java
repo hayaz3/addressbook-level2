576041d0d9ce5d8682749546f3d641235bcc8e0e
Haya Alsharef <haya.fayez@gmail.com>
2017-02-04 20:23:04 +0800

    create separate class for formatting job and modified both formatter and textUi to achieve SRP

--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -1,7 +1,6 @@
 package seedu.addressbook.ui;
 
 import static seedu.addressbook.common.Messages.*;
-
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
@@ -12,19 +11,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
-/**
- * Text UI of the application.
- */
-public class TextUi {
 
-    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+class Formatter {
+	
+	/** A decorative prefix added to the beginning of lines printed by AddressBook */
     private static final String LINE_PREFIX = "|| ";
 
     /** A platform independent line separator. */
     private static final String LS = System.lineSeparator();
 
     private static final String DIVIDER = "===================================================";
-
+    
     /** Format of indexed list item */
     private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
 
@@ -32,9 +29,81 @@ public class TextUi {
     /** Offset required to convert between 1-indexing and 0-indexing.  */
     public static final int DISPLAYED_INDEX_OFFSET = 1;
 
+	public String getUserCommand() {
+		
+		return LINE_PREFIX + "Enter command: ";
+		
+	}
+	
+	public String getFullUserCommand(String fullCommand){
+		
+		return "[Command entered:" + fullCommand + "]";
+	}
+
+	public String showToUser(String... message) {
+		
+		String intended = "";
+		
+		for(String m: message){
+			intended += LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX);
+		}
+		
+		return intended;
+		
+	}
+	
+	public String showWelcomeMessage(String version,String storageFilePath) {
+		String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
+		return showToUser(DIVIDER, DIVIDER, MESSAGE_WELCOME, version, 
+				MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE, storageFileInfo, DIVIDER);
+	}
+
+	public String getGoodByeMsg() {
+		return showToUser(MESSAGE_GOODBYE, DIVIDER, DIVIDER);
+		
+	}
+
+	public String getInitialFailMsg() {
+		return showToUser(MESSAGE_INIT_FAILED, DIVIDER, DIVIDER);
+	}
+
+	public String getResultForUser(CommandResult result) {
+		return showToUser(result.feedbackToUser, DIVIDER);
+	}
+	
+	/** Formats a list of strings as a viewable indexed list. */
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+	
+}
+
+/**
+ * Text UI of the application.
+ */
+public class TextUi {
+
+
     /** Format of a comment input line. Comment lines are silently consumed when reading user input. */
     private static final String COMMENT_LINE_FORMAT_REGEX = "#.*";
 
+    private Formatter format;
+    
     private final Scanner in;
     private final PrintStream out;
 
@@ -45,6 +114,7 @@ public class TextUi {
     public TextUi(InputStream in, PrintStream out) {
         this.in = new Scanner(in);
         this.out = out;
+        this.format = new Formatter();
     }
 
     /**
@@ -75,46 +145,36 @@ public class TextUi {
      * @return command (full line) entered by the user
      */
     public String getUserCommand() {
-        out.print(LINE_PREFIX + "Enter command: ");
+    	out.print(format.getUserCommand());
         String fullInputLine = in.nextLine();
 
         // silently consume all ignored lines
         while (shouldIgnore(fullInputLine)) {
             fullInputLine = in.nextLine();
         }
-
-        showToUser("[Command entered:" + fullInputLine + "]");
+        
+        out.println(format.showToUser(format.getFullUserCommand(fullInputLine)));
         return fullInputLine;
     }
 
 
     public void showWelcomeMessage(String version, String storageFilePath) {
-        String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
-        showToUser(
-                DIVIDER,
-                DIVIDER,
-                MESSAGE_WELCOME,
-                version,
-                MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE,
-                storageFileInfo,
-                DIVIDER);
+        out.println(format.showToUser(version, storageFilePath));
     }
 
     public void showGoodbyeMessage() {
-        showToUser(MESSAGE_GOODBYE, DIVIDER, DIVIDER);
+        out.println(format.getGoodByeMsg());
     }
 
 
     public void showInitFailedMessage() {
-        showToUser(MESSAGE_INIT_FAILED, DIVIDER, DIVIDER);
+    	out.println(format.getInitialFailMsg());
     }
 
     /** Shows message(s) to the user */
-    public void showToUser(String... message) {
-        for (String m : message) {
-            out.println(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX));
-        }
-    }
+//    public void showToUser(String... message) {
+//        out.println(format.showToUser(message));
+//    }
 
     /**
      * Shows the result of a command execution to the user. Includes additional formatting to demarcate different
@@ -125,7 +185,7 @@ public class TextUi {
         if (resultPersons.isPresent()) {
             showPersonListView(resultPersons.get());
         }
-        showToUser(result.feedbackToUser, DIVIDER);
+        out.println(format.getResultForUser(result));
     }
 
     /**
@@ -142,27 +202,11 @@ public class TextUi {
 
     /** Shows a list of strings to the user, formatted as an indexed list. */
     private void showToUserAsIndexedList(List<String> list) {
-        showToUser(getIndexedListForViewing(list));
+        out.println(format.showToUser(getIndexedListForViewing(list)));
     }
 
-    /** Formats a list of strings as a viewable indexed list. */
-    private static String getIndexedListForViewing(List<String> listItems) {
-        final StringBuilder formatted = new StringBuilder();
-        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
-        for (String listItem : listItems) {
-            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
-            displayIndex++;
-        }
-        return formatted.toString();
-    }
-
-    /**
-     * Formats a string as a viewable indexed list item.
-     *
-     * @param visibleIndex visible index for this listing
-     */
-    private static String getIndexedListItem(int visibleIndex, String listItem) {
-        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    public static String getIndexedListForViewing(List<String> listItems) {
+    	return Formatter.getIndexedListForViewing(listItems);
     }
 
 }
